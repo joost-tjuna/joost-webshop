37358480b24d2f2701f3db8838aafa3640abf4cd
Joost Tiemstra <joost@tjuna.com>
2017-03-30 15:08:07 +0200

    updated a migration so that admin collum is now added to the database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('admin');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('remember_token');
