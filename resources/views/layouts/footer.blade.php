492fd1eff23eb56f59bea189db8d9a10ef94ac82
Joost Tiemstra <joost@tjuna.com>
2017-04-11 11:05:13 +0200

    changed some text in the footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,5 +3,5 @@
     <p class="float-right"><a href="http://facebook.com">facebook</a></p>
     <p class="float-right"><a href="http://twitter.com">twitter</a> &middot; &nbsp;</p>
     <p class="float-right"><a href="#">Back to top</a> &middot; &nbsp;</p>
-    <p>&copy; 2017 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p>&copy; 2017 Joost Tiemstra.</p>
 </footer>
