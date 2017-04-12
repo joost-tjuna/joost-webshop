2ec7ed32c76d830eefbef8e85a2035a3a0d22654
Joost Tiemstra <joost@tjuna.com>
2017-04-12 12:09:18 +0200

    updatet the password resets to look nice and actually work

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+                <div class="panel-heading"><h2 class="header">Reset Password</h2></div>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
